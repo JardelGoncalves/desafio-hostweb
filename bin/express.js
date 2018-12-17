c977af95980af29e3b8327e9f0ae951ebba2511b
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-12-17 16:49:17 -0300

    add configurações do app (API)

---
+++ bin/express.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const express = require("express");
+const body_parser = require("body-parser");
+const app = express()
+
+// Configurações do body-parser
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended:true}));
+app.set("port", 3000);
+
+// Rotas API
+
+module.exports = app;
