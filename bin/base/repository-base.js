3289d9b915f0feb1c2643cafb54c9f5a773a7133
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-12-18 03:18:29 -0300

    add metodo delete e adptando

--- bin/base/repository-base.js
+++ bin/base/repository-base.js
@@ -28,13 +28,13 @@ module.exports.getAll = (table, projection) => {
 }
 
 // Obtem um usuário pelo id
-module.exports.getById = (table, id, projection) => {
+module.exports.getById = (table, id, message, projection) => {
     return new Promise((resolve, reject) => {
         connection.query(Query.findById(table, id, projection), (err, result) => {
             if (result) {
                 if (result.length <= 0) {
                     reject({
-                        message: "Nenhum usuário encontrado!",
+                        message: message,
                         status: 404
                     })
                 } else {
@@ -44,6 +44,32 @@ module.exports.getById = (table, id, projection) => {
                     })
                 }
 
+            } else {
+                console.log(err)
+                reject({
+                    message: "Ocorreu um erro inesperado!",
+                    status: 503
+                })
+            }
+        })
+    })
+}
+
+module.exports.delete = (table, id, message_202, message_404) => {
+    return new Promise((resolve, reject) => {
+        connection.query(Query.findByIdAndRemove(table, id), (err, result) => {
+            if (result) {
+                if (result.affectedRows === 1) {
+                    resolve({
+                        message: message_202,
+                        status: 202
+                    })
+                } else {
+                    reject({
+                        message: message_404,
+                        status: 404
+                    })
+                }
             } else {
                 console.log(err)
                 reject({
