87935321c9959706dfb557999f84cd3a233e4fa4
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-12-18 03:21:13 -0300

    criando endpoints

---
+++ routes/tarefa-routes.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const router = require("express").Router();
+const controller = require("../controllers/tarefa-controller");
+const auth = require("../middlewares/authentication");
+
+router.get("/", auth, controller.getAll);
+router.get("/:id", auth, controller.getById);
+router.post("/", auth, controller.post);
+router.put("/:id", auth, controller.put);
+router.put("/status/:id", auth, controller.alterarStatus);
+router.delete("/:id", auth, controller.delete);
+
+module.exports = router;
