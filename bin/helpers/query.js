dce489b14d0c859b3a2b3c4d5bd4a151807bd0aa
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-12-18 00:36:08 -0300

    add metodo findOne para consultas no db

--- bin/helpers/query.js
+++ bin/helpers/query.js
@@ -6,9 +6,9 @@
 
  // query para inserir dados no banco
  // recebe um objeto e a tabela onde o dado será inserido
-module.exports.save = (obj, table) => {
+module.exports.save = (data, table) => {
     // método concatenador retorna um objeto com os campos e values concatenados
-    let valores_concatenados = concatenador(obj, "insert")
+    let valores_concatenados = concatenador(data, "insert")
     return `INSERT INTO ${table} (${valores_concatenados.fields}) VALUES (${valores_concatenados.values})`
 }
 
@@ -50,7 +50,13 @@ module.exports.findByIdAndRemove = (table, id) => {
     return `DELETE FROM ${table} WHERE id='${id}'`
 }
 
-
+module.exports.findOne = (table, data, operator, projection) =>{
+    let valores_concatenados = concatenador(data,"select-one", operator);
+    if (!projection) {
+        return `SELECT * FROM ${table} WHERE ${valores_concatenados.select_one}'`
+    }
+    return `SELECT ${projection} FROM ${table} WHERE ${valores_concatenados.select_one}`
+}
 
 // método usado para retorna o tamanho de um objeto
 function getTamanho(obj) {
@@ -62,12 +68,13 @@ function getTamanho(obj) {
 }
 
 // método usado para concatenar os dados passado como objeto
-function concatenador(obj, tipo) {
+function concatenador(obj, tipo, operator) {
     // os dados retornados (fields e values são usados no insert e set usado no update)
     let info = {
         fields: "",
         values: "",
-        set: ""
+        set: "",
+        select_one:""
     };
     let tamanho = getTamanho(obj) - 1; // retorna a quantidade de posições corretas
     let cont = 0;
@@ -90,6 +97,16 @@ function concatenador(obj, tipo) {
                 info.set += properties + "=" + "'" + obj[properties] + "'," + " ";
             } else {
                 info.set += properties + "=" + "'" + obj[properties] + "'";
+            }
+            cont += 1
+        }
+    } else if (tipo === "select-one"){
+        // concatena para o operador passado como argumento
+        for (let properties in obj) {
+            if (cont < tamanho) {
+                info.select_one += properties + "=" + "'" + obj[properties] + "' " + operator + " ";
+            } else {
+                info.select_one += properties + "=" + "'" + obj[properties] + "'";
             }
             cont += 1
         }
