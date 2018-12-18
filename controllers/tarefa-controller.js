7cbe2dbd2426fd6c8e94b0e123732f65279ea89c
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-12-18 03:20:58 -0300

    add controller do serviço (endpoint) tarefas

---
+++ controllers/tarefa-controller.js
@@ -0,0 +1,87 @@
+"use strict"
+
+const _repository = require("../repositories/tarefa-repository");
+
+module.exports.getAll = (req, res) => {
+    _repository.getAll()
+        .then((success) => {
+            res.status(success.status).json(success.message)
+        })
+        .catch((failed) => {
+            res.status(failed.status).json({
+                message: failed.message
+            })
+        })
+}
+
+module.exports.getById = (req, res) => {
+    _repository.getById(req.params.id)
+        .then((success) => {
+            res.status(success.status).json(success.message)
+        })
+        .catch((failed) => {
+            res.status(failed.status).json({
+                message: failed.message
+            })
+        })
+}
+
+module.exports.post = (req, res) => {
+    _repository.post(req.body)
+        .then((success) => {
+            res.status(success.status).json({
+                message: success.message
+            })
+        })
+        .catch((failed) => {
+            res.status(failed.status).json({
+                message: failed.message,
+                erros: failed.erros
+            })
+        })
+}
+
+module.exports.put = (req, res) => {
+    _repository.put(req.params.id, req.body)
+        .then((success) => {
+            res.status(success.status).json({
+                message: success.message
+            })
+        })
+        .catch((failed) => {
+            res.status(failed.status).json({
+                message: failed.message,
+                erros: failed.erros
+            })
+        })
+}
+
+module.exports.alterarStatus = (req, res) => {
+    _repository.alteraStatus(req.params.id, {concluida: req.body.concluida})
+        .then((success) => {
+            res.status(success.status).json({
+                message: success.message
+            })
+        })
+        .catch((failed) => {
+            res.status(failed.status).json({
+                message: failed.message,
+                erros: failed.erros
+            })
+        })
+}
+
+module.exports.delete = (req, res)=>{
+    _repository.delete(req.params.id)
+    .then((success) => {
+        res.status(success.status).json({
+            message: success.message
+        })
+    })
+    .catch((failed) => {
+        res.status(failed.status).json({
+            message: failed.message,
+            erros: failed.erros
+        })
+    })
+}
