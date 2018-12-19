5313bdbe2c0960184c7df143a94321d656d7bc41
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-12-18 21:49:57 -0300

    add motodo post padrão para os demais repository

--- bin/base/repository-base.js
+++ bin/base/repository-base.js
@@ -1,6 +1,7 @@
 const Query = require("../helpers/query");
 const connection = require("../config/db-connection")();
 const Promise = require("promise");
+const md5 = require("md5");
 
 /**
  * Este modúlo possui os métodos repetidos dos repositories
@@ -28,13 +29,13 @@ module.exports.getAll = (table, projection) => {
 }
 
 // Obtem um usuário pelo id
-module.exports.getById = (table, id, message, projection) => {
+module.exports.getById = (table, id, projection) => {
     return new Promise((resolve, reject) => {
         connection.query(Query.findById(table, id, projection), (err, result) => {
             if (result) {
                 if (result.length <= 0) {
                     reject({
-                        message: message,
+                        message: table + " não encontrado!",
                         status: 404
                     })
                 } else {
@@ -55,18 +56,90 @@ module.exports.getById = (table, id, message, projection) => {
     })
 }
 
-module.exports.delete = (table, id, message_202, message_404) => {
+
+module.exports.post = (_validador, data, table) => {
+    return new Promise((resolve, reject) => {
+
+        if (_validador.erros.length === 0) {
+
+            // Query é um modulo criado para facilitar as consultas no db
+            connection.query(Query.save(data, table), (err, result) => {
+                if (result) {
+                    resolve({
+                        message: "Cadastrado realizado com sucesso",
+                        status: 201
+                    })
+                } else {
+                    console.log(err)
+                    reject({
+                        message: "Ocorreu um erro inesperado!",
+                        status: 503
+                    })
+                }
+            })
+
+        } else {
+            reject({
+                message: "Não foi possivel finalizar o cadastro",
+                erros: _validador.erros,
+                status: 400
+            })
+        }
+    })
+}
+
+
+module.exports.put = (_validador, id, data, table) => {
+    return new Promise((resolve, reject) => {
+
+        if (_validador.erros.length === 0) {
+            connection.query(Query.findByIdAndUpdate(table, id, data), (err, result) => {
+
+                if (result) {
+                    if (result.affectedRows === 1) {
+                        resolve({
+                            message: "Atualizado com sucesso!",
+                            status: 202
+                        })
+                    } else {
+                        reject({
+                            message: "Usuário não encontrado",
+                            status: 404
+                        })
+                    }
+                } else {
+                    console.log(err)
+                    reject({
+                        message: "Ocorreu um erro inesperado!",
+                        status: 503
+                    })
+                }
+
+            })
+        } else {
+            reject({
+                message: "Não foi possivel atualzar seus dados!",
+                erros: _validador.erros,
+                status: 400
+            })
+        }
+    })
+}
+
+
+
+module.exports.delete = (table, id) => {
     return new Promise((resolve, reject) => {
         connection.query(Query.findByIdAndRemove(table, id), (err, result) => {
             if (result) {
                 if (result.affectedRows === 1) {
                     resolve({
-                        message: message_202,
+                        message: "Removido com sucesso!",
                         status: 202
                     })
                 } else {
                     reject({
-                        message: message_404,
+                        message: "Não encontrado!",
                         status: 404
                     })
                 }
